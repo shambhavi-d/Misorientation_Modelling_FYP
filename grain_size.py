25ecf144a1942e1551bf62e2451d8e1a9dc22025
ProfCalc <chaudhuritamaghna@gmail.com>
2023-10-17 19:01:16 +0530

    Grainsize math corrected

--- grain_size.py
+++ grain_size.py
@@ -2,8 +2,8 @@ import numpy as np
 import main
 import random
 
-n1 = 50  ### Number of X which should be used for avg 
-n2 = 50 ### Number of Y which should be used for avg 
+n1 = 20  ### Number of X which should be used for avg 
+n2 = 20 ### Number of Y which should be used for avg 
 
 
 
@@ -13,13 +13,18 @@ def average_grain_x(n1):
     for y in random_y:
         i=0
         for x in range (0,main.r):
-            if main.s[x,y,3] < 0.1 or main.s[x+1,y,3] < 0.1:
+            if main.s[x,y,3] < 0 or main.s[x+1,y,3] < 0:
                 pass
-            elif 0< np.degrees(main.theta(np.matmul(main.G[x,y,0],main.G[x+1,y,1]))) < 15:
+            elif  np.degrees(main.theta(np.matmul(main.G[x,y,0],main.G[x+1,y,1]))) > 15:
                 i+=1
-        subgrains.append(i)
-        
-    return sum(subgrains)/(n1*main.r*main.stepsize_x)
+        #print(y)        
+        #print(i)          
+        #print(main.r*main.stepsize_x/i)        
+        subgrains.append(main.r*main.stepsize_x/i)
+    
+    #print(subgrains)
+    #print(main.r*main.stepsize_x)    
+    return sum(subgrains)/len(subgrains)
     
 def average_grain_y(n2):
     random_x = random.sample(range(0,main.r),n2)
@@ -27,13 +32,17 @@ def average_grain_y(n2):
     for x in random_x:
         i=0
         for y in range (0,main.c):
-            if main.s[x,y,3] < 0.1 or main.s[x,y+1,3] < 0.1:
+            if main.s[x,y,3] < 0 or main.s[x,y+1,3] < 0:
                 pass
-            elif 0 < np.degrees(main.theta(np.matmul(main.G[x,y,0],main.G[x,y+1,1]))) < 15:
+            elif np.degrees(main.theta(np.matmul(main.G[x,y,0],main.G[x,y+1,1]))) > 15:
                 i+=1
-        subgrains.append(i)
-        
-    return sum(subgrains)/(n2*main.c*main.stepsize_y)
+        #print(x)        
+        #print(i)        
+        #print(main.r*main.stepsize_y/i) 
+        subgrains.append(main.c*main.stepsize_y/i)
+    #print(subgrains)    
+    #print(main.r*main.stepsize_y)  
+    return sum(subgrains)/len(subgrains)
 
 if __name__ == "__main__":
     f = open("grain_size.txt", "w" )
